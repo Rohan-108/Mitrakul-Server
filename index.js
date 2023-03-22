b90861b85ee567b74b2540cbdb17931ce428e29f
Rohan Jha <rohan21_ug@ece.nits.ac.in>
2023-03-22 20:11:45 +0530

    ADD: socket.io code added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { Server } from "socket.io";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
@@ -16,10 +17,12 @@ app.use("/api/chats", chatRouter);
 app.use("/api/message", messageRouter);
 app.use(notFound);
 app.use(errorHandler);
+let server;
+const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(5000, () =>
+    server = app.listen(PORT, () =>
       console.log("server started at http://localhost:5000")
     );
   } catch (error) {
@@ -27,3 +30,31 @@ const startServer = async () => {
   }
 };
 startServer();
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    socket.emit("connected" + room);
+  });
+  socket.on("new message", (messageReceived) => {
+    const chat = messageReceived.chat;
+    if (!chat.users) return console.log("chat users dont exist");
+    chat.users.forEach((user) => {
+      if (user._id == messageReceived.sender._id) return;
+      socket.in(user._id).emit("message recieved", messageReceived);
+    });
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+});
